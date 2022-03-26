57a8a7a8243b6a288e629bb8445f6276df6f086c
Simarjit <simarjassal9@gmail.com>
2022-03-25 23:25:27 -0400

    length of String

--- StringLength.java
+++ StringLength.java
@@ -7,10 +7,8 @@ public class StringLength {
 		String S1 = "Hello My name is Simar";
 		String S2 = "I live in Guelph";
 
-		// length() method of String returns the length of a String S1.
-		int length = S1.length();
-		System.out.println("Length of a String 1 is: " + length);
-		// 8 Length of a String RockStar
+		System.out.println("Length of a String 1 is: " + S1.length());
+
 		System.out.println("Length of a String 2 is: " + S2.length());
 	}
 
