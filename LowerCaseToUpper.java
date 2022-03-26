4ae5c6cc189317400e810d77568ef030be769e5f
Simarjit <simarjassal9@gmail.com>
2022-03-25 23:12:48 -0400

    lower case to uppercase

---
+++ LowerCaseToUpper.java
@@ -0,0 +1,13 @@
+package practice;
+//Lower case to Upper case in String
+public class LowerCaseToUpper {
+
+	public static void main(String[] args) {
+
+		String str = "simarjit";
+
+		System.out.println("String from lower case to upper case is: " + str.toUpperCase());
+
+	}
+
+}
